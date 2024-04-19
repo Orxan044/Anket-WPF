0686a5b7233b24e38e298b9d131599d8d581c2d2
Husey_so01 <Husey_so01@STHQ0124-12>
2024-04-19 12:00:40 +0400

    Add project files.

---
+++ Anket/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Anket;
+
+public partial class MainWindow : Window
+{
+    private User _user;
+    string? dateGender;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void btnLoad_Click(object sender, RoutedEventArgs e)
+    {
+        string fileName = $"{txtBoxLoad}.json";
+        string text = File.ReadAllText(fileName);
+        var json = JsonSerializer.Deserialize<User>(text);
+
+        txtBoxFatherName.Text = json.FatherName;
+        txtBoxName.Text = json.Name;
+        txtBoxSurname.Text = json.Surname;
+        txtBoxCountry.Text = json.Country;
+        txtBoxCity.Text = json.City;
+        if(rbMale.Content.ToString() == json.Gender) rbMale.IsChecked = true;
+        else rbFemale.IsChecked = true;
+        Calendar.Text = json.Brithday.ToString();
+        
+    }
+
+    private void btnSave_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            _user = new()
+            {
+                FatherName = txtBoxFatherName.Text,
+                Name = txtBoxName.Text,
+                Surname = txtBoxSurname.Text,
+                Country = txtBoxCountry.Text,
+                City = txtBoxCity.Text,
+                Phone = txtBoxPhone.Text,
+                Gender = dateGender,
+                Brithday = Calendar.ToString(),
+            };
+
+            string fileName = $"{txtBoxName}.json";
+            string json = JsonSerializer.Serialize(_user);
+            File.WriteAllText(fileName, json);
+            MessageBox.Show("Saveing User");
+        }
+        catch (System.Exception)
+        {
+
+            MessageBox.Show("Eror Var");
+        }
+    }
+
+    private void RadioButton_Checked(object sender, RoutedEventArgs e)
+    {
+        if(sender is RadioButton rb)
+        {
+            if (rb.Content.ToString() == "Male") dateGender = "Male";
+            if (rb.Content.ToString() == "Female") dateGender = "Female";
+        }
+    }
+}
---
+++ Anket/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Anket;
+
+internal class User
+{
+    public string? FatherName { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public string? Phone { get; set; }
+    public string? Brithday { get; set; }
+    public string? Gender { get; set; }
+
+   // public override string ToString() => $"Father Name:{}"
+}
